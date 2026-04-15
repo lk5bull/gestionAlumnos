bbf7d5ca2cf933f939f5d2c157882a9163a5fad0
salvador juarez <101913799+shadownrx@users.noreply.github.com>
2026-04-14 21:03:21 -0300

    add .env & server

--- backend/Src/index.js
+++ backend/Src/index.js
@@ -1,2 +1,14 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(4000, () => {
+    console.log("Server is running on port 4000");
+});
